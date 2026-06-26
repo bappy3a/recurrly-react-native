7a3c367b4019c95552770f0db406554ad137042d
Ahmed Bappy <bappy3a@gmail.com>
2026-06-27 01:51:58 +0600

    update

---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class onboarding extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Onboarding </Text>
+      </View>
+    )
+  }
+}
