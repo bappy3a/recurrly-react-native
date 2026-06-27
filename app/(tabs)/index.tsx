1136d84e82f7aaa168f2c721ade4dc86401b4ba0
Ahmed Bappy <bappy3a@gmail.com>
2026-06-27 23:12:40 +0600

    feat: refactor home screen layout for improved styling and structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,26 @@
 import { HOME_BALANCE, HOME_USER } from "@/constants/data";
 import { icons } from "@/constants/icons";
 import images from "@/constants/images";
+import { colors } from "@/constants/theme";
 import { formatCurrency } from "@/lib/utils";
 import dayjs from "dayjs";
-import { Image, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
+    <SafeAreaView style={styles.safeArea}>
+      <View className="flex-1 p-5">
+        <View className="home-header">
+          <View className="home-user">
+            <Image source={images.avatar} className="home-avatar" />
+            <Text className="home-user-name">{HOME_USER.name}</Text>
+          </View>
 
-    <SafeAreaView className="flex-1 bg-background p-5">
-      <View className="home-header">
-        <View className="home-user">
-          <Image source={images.avatar} className="home-avatar"  />
-          <Text className="home-user-name" >{HOME_USER.name}</Text>
+          <Image source={icons.add} className="home-add-icon" />
         </View>
 
-        <Image source={icons.add} className="home-add-icon" />
-      </View>
-
-      <View className="home-balance-card">
+        <View className="home-balance-card">
           <Text className="home-balance-label">Balance</Text>
           <View className="home-balance-row">
             <Text className="home-balance-amount">{ formatCurrency(HOME_BALANCE.amount) }</Text>
@@ -27,8 +28,15 @@ export default function Index() {
               {dayjs(HOME_BALANCE.nextRenewalDate).format('MM/DD')}
             </Text>
           </View>
+        </View>
       </View>
-
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+});
