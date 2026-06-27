789e7e0bb62689b86b0da403144e3c37704cd516
Ahmed Bappy <bappy3a@gmail.com>
2026-06-27 23:08:50 +0600

    feat: enhance home screen layout and functionality with user balance and subscription management

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,34 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { HOME_BALANCE, HOME_USER } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
 
-    <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href={"/onboarding"} className="mt-4 rounded bg-primary text-white p-4" >Onborad</Link>
-    </View>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar"  />
+          <Text className="home-user-name" >{HOME_USER.name}</Text>
+        </View>
+
+        <Image source={icons.add} className="home-add-icon" />
+      </View>
+
+      <View className="home-balance-card">
+          <Text className="home-balance-label">Balance</Text>
+          <View className="home-balance-row">
+            <Text className="home-balance-amount">{ formatCurrency(HOME_BALANCE.amount) }</Text>
+            <Text className="home-balance-date">
+              {dayjs(HOME_BALANCE.nextRenewalDate).format('MM/DD')}
+            </Text>
+          </View>
+      </View>
+
+    </SafeAreaView>
   );
 }
